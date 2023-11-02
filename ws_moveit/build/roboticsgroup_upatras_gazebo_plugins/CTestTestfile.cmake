9f284e5f764dd788ae3d69e5561756e6e37c5831
Francisco-SP3 <A01177893@tec.mx>
2023-11-02 15:28:05 -0600

    xarm goes in workspace

--- ws_moveit/build/roboticsgroup_upatras_gazebo_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fsp/Roborregos/xarm_ros/ws_moveit/src/roboticsgroup_upatras_gazebo_plugins
-# Build directory: /home/fsp/Roborregos/xarm_ros/ws_moveit/build/roboticsgroup_upatras_gazebo_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
